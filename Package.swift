9f4185ece5b917c711f3fb20e3a01e739de0d8a6
Matthew Ryan <matt@souther.ly>
2021-01-18 15:29:13 +1100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,6 +20,6 @@ let package = Package(
         .package(name: "Swifter", url: "https://github.com/httpswift/swifter.git", .upToNextMajor(from: "1.5.0"))
     ],
     targets: [
-        .binaryTarget(name: "AlltKit", url: "https://.zip", checksum: "ecc2fcb2562b4605ab9610af29ce586ab82b392330585d5e5b87fc9ff14149a8")
+        .binaryTarget(name: "AlltKit", url: "https://github.com/allt-tv/AlltKit-SP/releases/download/v1.0.0-beta/AlltKit-1.0.0-beta.zip", checksum: "ecc2fcb2562b4605ab9610af29ce586ab82b392330585d5e5b87fc9ff14149a8")
     ]
 )
